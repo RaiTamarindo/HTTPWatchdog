0cd6b34ebbc3d895eb52f2e6c9e65bc2bea88495
Rai Tamarindo <raitamarindo@gmail.com>
2019-07-15 17:36:23 -0300

    Fix response time measurement

--- worker.js
+++ worker.js
@@ -2,24 +2,22 @@ var websiteModel = require('./api/models/websiteModel.js'),
     https = require('https'),
     http = require('http');
 
-const SAMPLE_TIME = 10000;
+const SAMPLE_TIME = 5000;
 
-function sendText(text)
-{
-    process
-        .send(
-        {
-            text: text
-        });
+function sendText(text) {
+    process.send({ text: text });
 }
 
-function sendData(data)
-{
-    process
-        .send(
-        {
-            data: data
-        });
+function sendData(data) {
+    process.send({ data: data });
+}
+
+function getDuration(t1, t0) {
+    if (!t0) {
+        t0 = [0, 0];
+    }
+
+    return t1[0]*1000 + t1[1]/1000000 - t0[0]*1000 - t0[1]/1000000;
 }
 
 function doMeasure(website)
@@ -28,42 +26,62 @@ function doMeasure(website)
     
     try
     {
-        const httpClient = website.url.startsWith('https:') ? https : http;
-        const options = { };
-        if (website.username) {
+        const url = new URL(website.url);
+        const httpClient = url.protocol === 'https:' ? https : http;
+        const options = {
+            method: 'GET',
+            host: url.host,
+            path: url.pathname,
+        };
+        if (!!website.username) {
             options.auth = `${website.username}:${website.password}`;
         }
-        const startDate = Date.now();
-        httpClient
-            .get(website.url, options, function(res)
-            {
-                website.lastResponseDate = Date.now();
-                website.lastResponseTime = website.lastResponseDate - startDate;
-                website.lastStatusCode = res.statusCode;
-                if(website.lastStatusCode >= 200 && website.lastStatusCode < 500)
-                {
-                    website.successfulResponses++;
-                }
-                if(website.lastResponseTime <= 400)
-                {
-                    website.fastResponses++;
-                }
-                
-                sendData(website);
-                websiteModel.modify(website._id, website);
-                sendText('[' + website.url + ']' +
-                         ' successful responses: ' + website.successfulResponses +
-                         ' fast responses: ' + website.fastResponses +
-                         ' (last: ' + website.lastResponseTime + ' ms)' +
-                         ' total requests: ' + website.totalRequests);
-            })
-            .on('error', function(e)
-            {
-                website.lastStatusCode = -1;
-                sendData(website);
-                websiteModel.modify(website._id, website);
-                sendText(e.message);
+        const timings = {
+            tcpConnectionAt: undefined,
+            tlsHandshakeAt: undefined,
+            firstByteAt: undefined,
+        };
+        const req = httpClient
+            .request(options, (res) => {
+                res.once('readable', () => {
+                    const now = process.hrtime();
+                    website.lastResponseDate = getDuration(now);
+                    website.lastResponseTime = getDuration(now, timings.tlsHandshakeAt || timings.tcpConnectionAt);
+
+                    website.lastStatusCode = res.statusCode;
+                    if(website.lastStatusCode >= 200 && website.lastStatusCode < 500)
+                    {
+                        website.successfulResponses++;
+                    }
+                    if(website.lastResponseTime <= 200)
+                    {
+                        website.fastResponses++;
+                    }
+                    
+                    sendData(website);
+                    websiteModel.modify(website._id, website);
+                    sendText('[' + website.url + ']' +
+                             ' successful responses: ' + website.successfulResponses +
+                             ' fast responses: ' + website.fastResponses +
+                             ' (last: ' + website.lastResponseTime + ' ms)' +
+                             ' total requests: ' + website.totalRequests);
+                })
             });
+        req.on('socket', (socket) => {
+            socket.on('connect', () => {
+                timings.tcpConnectionAt = process.hrtime();
+            });
+            socket.on('secureConnect', () => {
+                timings.tlsHandshakeAt = process.hrtime();
+            });
+        });
+        req.on('error', (e) => {
+            website.lastStatusCode = -1;
+            sendData(website);
+            websiteModel.modify(website._id, website);
+            sendText(e.message);
+        });
+        req.end();
     }
     catch(e)
     {
